1db41b2bab0464809e055a34f6fa380c9af23d64
Eetu Pyykko <eetu.o.pyykko@student.laurea.fi>
2026-09-10 14:00:24 +0300

    Working on workshops

---
+++ WS02_JS_basics/js/script.js
@@ -0,0 +1,37 @@
+const name = "Eetu";
+let age = 22;
+const favouriteAnimal = "Kissa";
+
+console.log(name);
+console.log(age);
+console.log(favouriteAnimal);
+
+const sentence = `Hei! Nimeni on ${name} ja lempieläimeni on ${favouriteAnimal}.`;
+console.log(sentence);
+
+const userName = prompt("Mikä on nimesi?");
+console.log(`Hei ${userName}! Tervetuloa JavaScriptin maailmaan!`);
+
+const userAgeInput = prompt("Kuinka vanha olet?");
+const userAge = Number(userAgeInput);
+
+if (userAge >= 18) {
+  console.log("Olet aikuinen.");
+} else {
+  console.log("Olet alaikäinen.");
+}
+
+function greetUser(name) {
+  console.log("Hei " + name + "!");
+}
+
+greetUser("John");
+greetUser("Emma");
+greetUser("Matti");
+
+const helloButton = document.getElementById("helloButton");
+
+helloButton.addEventListener("click", function () {
+  alert("JavaScript toimii! Nappia painettu.");
+  console.log("Nappia painettu – JS on yhdistetty sivuun.");
+});
